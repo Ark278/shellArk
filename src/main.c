0e7e0bfb29539afb44d4ef7fba02791205a89bae
Ark278 <abhinav.raj278@gmail.com>
2026-06-14 13:04:31 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   // TODO: Uncomment the code below to pass the first stage
   char buffer[100] = {0};
-  printf("$ ");
-  scanf ("%99s", buffer); 
-  printf("%s: command not found", buffer);
+  bool flag = true;
+  while(flag) {
+    printf("$ ");
+    fflush(stdout);
+    scanf ("%99s", buffer); 
+    printf("%s: command not found\n", buffer);
+  }
   return 0;
 }
