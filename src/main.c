bcdcc5875c6fb6d30ef7ae4120c45afa057d7c71
Ark278 <abhinav.raj278@gmail.com>
2026-06-14 21:10:10 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,15 +6,16 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   // TODO: Uncomment the code below to pass the first stage
+  setbuf(stdout, NULL);
   char buffer[100] = {0};
   while(strcmp(buffer, "exit")) {
     printf("$ ");
     fflush(stdout);
-    scanf ("%99s", buffer);
-    if(strcmp(buffer, "echo") == 0) {
-      char echo_buffer[100] = {0};
-      fgets(echo_buffer, 100, stdin);
-      printf("%s", echo_buffer);
+    
+    fgets(buffer, 100, stdin);
+    buffer[strlen(buffer) - 1] = '\0';
+    if(strncmp(buffer, "echo ", 5) == 0) {
+      printf("%s\n", buffer + 5);
       continue;
     }
     printf("%s: command not found\n", buffer);
