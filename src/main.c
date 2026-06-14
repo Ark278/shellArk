867207cef89082ab6ee211f387a1a6383fcebe6d
Ark278 <abhinav.raj278@gmail.com>
2026-06-14 12:49:08 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,7 +6,9 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
   // TODO: Uncomment the code below to pass the first stage
+  char buffer[100] = 0;
   printf("$ ");
-
+  scanf ("%99s", buffer); 
+  printf("{%s}: command not found", buffer);
   return 0;
 }
