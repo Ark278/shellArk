1b3dcdee179be67d8733e69bc8a67efbbfc4d71b
Ark278 <abhinav.raj278@gmail.com>
2026-06-14 12:51:38 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,10 +3,8 @@
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
-  setbuf(stdout, NULL);
-
   // TODO: Uncomment the code below to pass the first stage
-  char buffer[100] = 0;
+  char buffer[100] = {0};
   printf("$ ");
   scanf ("%99s", buffer); 
   printf("{%s}: command not found", buffer);
