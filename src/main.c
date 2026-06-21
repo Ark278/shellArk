aec616ee9ecc37598d717c0cfeecde7e96846294
Ark278 <abhinav.raj278@gmail.com>
2026-06-22 01:25:56 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -8,17 +8,16 @@ int main(int argc, char *argv[]) {
   // TODO: Uncomment the code below to pass the first stage
   setbuf(stdout, NULL);
   char buffer[100] = {0};
-  while(strcmp(buffer, "exit")) {
+  while(1) {
     printf("$ ");
     fflush(stdout);
-    
+    if(strcmp(buffer, "exit")    == 0) printf("%s: command not found\n", buffer);
     fgets(buffer, 100, stdin);
     buffer[strlen(buffer) - 1] = '\0';
     if(strncmp(buffer, "echo ", 5) == 0) {
       printf("%s\n", buffer + 5);
       continue;
     }
-    printf("%s: command not found\n", buffer);
   }
   return 0;
 }
